7dc25b64212eb0c6d0e07abcce0871eb2c857de2
154pinkchairs <mholub@tutanota.com>
2022-12-31 06:09:26 +0100

    feat(global): introduce history DB

--- go.mod
+++ go.mod
@@ -1,10 +1,11 @@
-module gowebcli
+module github.com/154pinkchairs/gowebcli
 
 go 1.19
 
 require github.com/rthornton128/goncurses v0.0.0-20220628231859-fd57939296e5
 
 require (
+	github.com/mattn/go-sqlite3 v1.14.16 // indirect
 	github.com/sirupsen/logrus v1.9.0 // indirect
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 )
--- main.go
+++ main.go
@@ -5,10 +5,108 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-
+	"strings"
+	hist "github.com/154pinkchairs/gowebcli/history"
 	nc "github.com/rthornton128/goncurses"
 	log "github.com/sirupsen/logrus"
 )
+
+type URL struct {
+	url string
+	inhistory bool
+}
+
+//query the history table for the values in url column at least partially matching the input string
+func (u *URL) MatchingHistory() ([]string, error) {
+	var urls []string
+
+	db, err := hist.NewHistoryDB("~/.local/share/gowebcli/history.db")
+	if err != nil {
+		return nil, err
+	}
+
+	rows, err := db.conn.Query("SELECT url FROM history WHERE url LIKE ?", u.url + "%")
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		var url string
+		err = rows.Scan(&url)
+		if err != nil {
+			return nil, err
+		}
+		urls = append(urls, url)
+	}
+
+	return urls, nil
+}
+
+func (u *URL) GetURL() string {
+		for {
+			c := urlBar.GetChar()
+			cursorY, cursorX := urlBar.CursorYX()
+			if c == nc.KEY_ENTER || c == 10 { // 10 is the ASCII code for a newline
+				if len(url) > 0 {
+					log.Info("Visiting:", url)
+					historyCounter++
+					break
+				} else {
+					continue
+				}
+			} else if c == nc.KEY_BACKSPACE || c == 127 {
+				if len(url) > 0 {
+					url = url[:len(url)-1]
+					urlBar.MovePrint(0, 0, "URL: "+url)
+					urlBar.Refresh()
+				} //end of backspace/del handling
+			} else if c == nc.KEY_DC {
+				urlBar.DelChar()
+				if err != nil {
+					log.Error(err)
+				}
+				urlBar.Refresh()
+			} else if c == nc.KEY_UP {
+				if historyIndex > 0 {
+					historyIndex--
+					url = history[historyIndex]
+					urlBar.MovePrint(0, 0, "URL: "+url)
+					urlBar.Refresh()
+				} //end of keyup handling
+			} else if c == nc.KEY_DOWN {
+				if historyIndex < len(history)-1 {
+					historyIndex++
+					url = history[historyIndex]
+					urlBar.MovePrint(0, 0, "URL: "+url)
+					urlBar.Refresh()
+				}
+			}	else if c == nc.KEY_RIGHT {
+					//move one character to the right until the end of the line
+					if cursorX < len(url)+5 {
+						urlBar.Move(cursorY, cursorX+1)
+						urlBar.Refresh()
+					}
+				} else if c == nc.KEY_LEFT {
+						if cursorX > 5 {
+							urlBar.Move(cursorY, cursorX-1)
+							urlBar.Refresh()
+						}
+					} else if c == nc.KEY_HOME {
+						urlBar.Move(cursorY, 5)
+						urlBar.Refresh()
+					} else if c == nc.KEY_END {
+						urlBar.Move(cursorY, len(url)+5)
+						urlBar.Refresh()
+				} else if c > 31 && c < 127 {
+					url += nc.KeyString(c)
+					urlBar.MovePrint(0, 0, "URL: "+url)
+				}
+				urlBar.Refresh()
+			}
+		}
+		return url
+	}
+}
 
 func main() {
 	f, err := os.OpenFile("gowebcli.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
@@ -17,7 +115,7 @@ func main() {
 	}
 	log.SetFormatter(&log.JSONFormatter{})
 	log.SetOutput(f)
-	log.SetLevel(log.DebugLevel)
+	log.SetLevel(log.InfoLevel)
 	stdscr, err := nc.Init()
 	if err != nil {
 		log.Fatal(err)
@@ -36,47 +134,15 @@ func main() {
 	}
 	defer urlBar.Delete()
 
-	//urlBarReader := bufio.NewReader(urlBar)
 	for {
 		urlBar.MovePrint(0, 0, "URL: ")
 		urlBar.Refresh()
 
 		var url string
-		var history []string
-		var historyIndex int
-		for {
-			c := urlBar.GetChar()
-			if c == nc.KEY_ENTER || c == 10 { // 10 is the ASCII code for a newline
-				if len(url) > 0 {
-					break
-				} else {
-					continue
-				}
-			} else if c == nc.KEY_BACKSPACE || c == 127 || c == nc.KEY_DC {
-				if len(url) > 0 {
-					url = url[:len(url)-1]
-					urlBar.MovePrint(0, 0, "URL: "+url)
-					urlBar.Refresh()
-				} //end of backspace/del handling
-			} else if c == nc.KEY_UP {
-				if historyIndex > 0 {
-					historyIndex--
-					url = history[historyIndex]
-					urlBar.MovePrint(0, 0, "URL: "+url)
-					urlBar.Refresh()
-				} //end of keyup handling
-			} else if c == nc.KEY_DOWN {
-				if historyIndex < len(history)-1 {
-					historyIndex++
-					url = history[historyIndex]
-					urlBar.MovePrint(0, 0, "URL: "+url)
-					urlBar.Refresh()
-				} else {
-					url += nc.KeyString(c)
-					urlBar.MovePrint(0, 0, "URL: "+url)
-					urlBar.Refresh()
-				}
-			}
+		historyCounter := 0
+		history := make([]string, historyCounter)
+		historyIndex := 0
+
 			resp, err := http.Get(url)
 			if err != nil {
 				log.Println(err)
