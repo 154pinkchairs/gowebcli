1a4f84ba486a3e880e5b4f6cdedba50a23dd9d0b
154pinkchairs <mholub@tutanota.com>
2022-12-31 07:59:04 +0100

    feat(db): add HDB interface
    feat(db): add HDB interface

---
+++ history/db.go
@@ -0,0 +1,150 @@
+package history
+
+import (
+	"database/sql"
+	"sync"
+	"time"
+
+	_ "github.com/mattn/go-sqlite3"
+	log "github.com/sirupsen/logrus"
+)
+
+type HistoryDB struct {
+	Conn *sql.DB
+	dsn  string
+	mux  *sync.Mutex
+}
+
+type HDB interface {
+	Add(url string, timestamp time.Time) error
+	Get(index int32) (*History, error)
+	GetAll() ([]*History, error)
+	Delete(index int32) error
+	DeleteAll() error
+	Count() (int32, error)
+	connect() error
+	Close() error
+}
+
+type History struct {
+	Index     int32
+	URL       string
+	Timestamp time.Time
+}
+
+func NewHistoryDB(dsn string) (*HistoryDB, error) {
+	db := &HistoryDB{
+		mux: &sync.Mutex{},
+		dsn: "~/.local/share/gowebcli/history.db",
+	}
+
+	if err := db.connect(); err != nil {
+		log.Fatal(err)
+	}
+
+	return db, nil
+}
+
+func (db *HistoryDB) connect() error {
+	var err error
+
+	db.Conn, err = sql.Open("sqlite3", db.dsn)
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Conn.Exec("CREATE TABLE IF NOT EXISTS history (index INTEGER PRIMARY KEY, url TEXT, timestamp INTEGER)")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (db *HistoryDB) Add(url string, timestamp time.Time) error {
+	db.mux.Lock()
+	defer db.mux.Unlock()
+	_, err := db.Conn.Exec("INSERT INTO history (url, timestamp) VALUES (?, ?)", url, timestamp)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (db *HistoryDB) Get(index int32) (*History, error) {
+	db.mux.Lock()
+	defer db.mux.Unlock()
+
+	var h History
+	err := db.Conn.QueryRow("SELECT * FROM history WHERE index = ?", index).Scan(&h.Index, &h.URL, &h.Timestamp)
+	if err != nil {
+		return nil, err
+	}
+
+	return &h, nil
+}
+
+func (db *HistoryDB) GetAll() ([]*History, error) {
+	db.mux.Lock()
+	defer db.mux.Unlock()
+
+	rows, err := db.Conn.Query("SELECT * FROM history")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var hs []*History
+	for rows.Next() {
+		var h History
+		if err := rows.Scan(&h.Index, &h.URL, &h.Timestamp); err != nil {
+			return nil, err
+		}
+		hs = append(hs, &h)
+	}
+
+	return hs, nil
+}
+
+func (db *HistoryDB) Delete(index int32) error {
+	db.mux.Lock()
+	defer db.mux.Unlock()
+
+	_, err := db.Conn.Exec("DELETE FROM history WHERE index = ?", index)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (db *HistoryDB) DeleteAll() error {
+	db.mux.Lock()
+	defer db.mux.Unlock()
+
+	_, err := db.Conn.Exec("DELETE FROM history")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// count counts the number of rows in the history table
+func (db *HistoryDB) Count() (int32, error) {
+	db.mux.Lock()
+	defer db.mux.Unlock()
+
+	var count int32
+	err := db.Conn.QueryRow("SELECT COUNT(*) FROM history").Scan(&count)
+	if err != nil {
+		return 0, err
+	}
+
+	return count, nil
+}
+
+func (db *HistoryDB) Close() error {
+	return db.Conn.Close()
+}
--- main.go
+++ main.go
@@ -5,18 +5,36 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"strings"
+	"time"
+
 	hist "github.com/154pinkchairs/gowebcli/history"
 	nc "github.com/rthornton128/goncurses"
 	log "github.com/sirupsen/logrus"
 )
 
-type URL struct {
-	url string
-	inhistory bool
+func SetupUI() (err error, browserWin *nc.Window, UrlBar *nc.Window) {
+	stdscr, err := nc.Init()
+	if err != nil {
+		log.Fatal(err)
+	}
+	h, w := stdscr.MaxYX()
+	BrowserWin, err := nc.NewWindow(h-1, w, 0, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	UB, err := nc.NewWindow(1, w, h-1, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return err, BrowserWin, UB
 }
 
-//query the history table for the values in url column at least partially matching the input string
+type URL struct {
+	urlAddr   string
+	timestamp time.Time
+}
+
+// query the history table for the values in url column at least partially matching the input string
 func (u *URL) MatchingHistory() ([]string, error) {
 	var urls []string
 
@@ -24,8 +42,9 @@ func (u *URL) MatchingHistory() ([]string, error) {
 	if err != nil {
 		return nil, err
 	}
+	defer db.Close()
 
-	rows, err := db.conn.Query("SELECT url FROM history WHERE url LIKE ?", u.url + "%")
+	rows, err := db.Conn.Query("SELECT url FROM history WHERE url LIKE ?", u.urlAddr+"%")
 	if err != nil {
 		return nil, err
 	}
@@ -42,70 +61,122 @@ func (u *URL) MatchingHistory() ([]string, error) {
 	return urls, nil
 }
 
+func (u *URL) GetURLFromHistory(index int32) string {
+	var url string
+
+	db, err := hist.NewHistoryDB("~/.local/share/gowebcli/history.db")
+	if err != nil {
+		log.Error(err)
+	}
+	defer db.Close()
+
+	history, err := db.Get(index)
+	if err != nil {
+		log.Info(err)
+	}
+
+	url = history.URL
+
+	return url
+}
+
+func (u *URL) AddToHistory() error {
+	db, err := hist.NewHistoryDB("~/.local/share/gowebcli/history.db")
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	err = db.Add(u.urlAddr, u.timestamp)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func (u *URL) GetURL() string {
-		for {
-			c := urlBar.GetChar()
-			cursorY, cursorX := urlBar.CursorYX()
-			if c == nc.KEY_ENTER || c == 10 { // 10 is the ASCII code for a newline
-				if len(url) > 0 {
-					log.Info("Visiting:", url)
-					historyCounter++
+	db, err := hist.NewHistoryDB("~/.local/share/gowebcli/history.db")
+	if err != nil {
+		log.Error(err)
+	}
+	defer db.Close()
+	err, _, UrlBar := SetupUI()
+	if err != nil {
+		log.Fatal(err)
+	}
+	var historyIdxCur int32
+	var urlAddr string
+	histLen, err := hist.HDB.Count(db)
+	if err != nil {
+		log.Errorf("Error getting history length: %v", err)
+	}
+	for {
+		c := UrlBar.GetChar()
+		cursorY, cursorX := UrlBar.CursorYX()
+		if c == nc.KEY_ENTER || c == 10 { // 10 is the ASCII code for a newline
+			if len(urlAddr) > 0 {
+				log.Info("Visiting:", urlAddr)
+				u.urlAddr = urlAddr
+				u.timestamp = time.Now()
+				err = u.AddToHistory()
+				if err != nil {
+					log.Errorf("Error adding \"%s\" to history: %v", urlAddr, err)
 					break
 				} else {
 					continue
 				}
 			} else if c == nc.KEY_BACKSPACE || c == 127 {
-				if len(url) > 0 {
-					url = url[:len(url)-1]
-					urlBar.MovePrint(0, 0, "URL: "+url)
-					urlBar.Refresh()
+				if len(urlAddr) > 0 {
+					urlAddr = urlAddr[:len(urlAddr)-1]
+					UrlBar.MovePrint(0, 0, "urlAddr: "+urlAddr)
+					UrlBar.Refresh()
 				} //end of backspace/del handling
 			} else if c == nc.KEY_DC {
-				urlBar.DelChar()
+				UrlBar.DelChar()
 				if err != nil {
 					log.Error(err)
 				}
-				urlBar.Refresh()
+				UrlBar.Refresh()
 			} else if c == nc.KEY_UP {
-				if historyIndex > 0 {
-					historyIndex--
-					url = history[historyIndex]
-					urlBar.MovePrint(0, 0, "URL: "+url)
-					urlBar.Refresh()
+				if historyIdxCur > 0 {
+					historyIdxCur++
+					urlAddr = u.GetURLFromHistory(historyIdxCur)
+					UrlBar.MovePrint(0, 0, "urlAddr: "+urlAddr)
+					UrlBar.Refresh()
 				} //end of keyup handling
 			} else if c == nc.KEY_DOWN {
-				if historyIndex < len(history)-1 {
-					historyIndex++
-					url = history[historyIndex]
-					urlBar.MovePrint(0, 0, "URL: "+url)
-					urlBar.Refresh()
+				if historyIdxCur < histLen {
+					historyIdxCur--
+					urlAddr = u.GetURLFromHistory(historyIdxCur)
+					UrlBar.MovePrint(0, 0, "urlAddr: "+urlAddr)
+					UrlBar.Refresh()
+				} //end of keydown handling
+			} else if c == nc.KEY_RIGHT {
+				//move one character to the right until the end of the line
+				if cursorX < len(urlAddr)+5 {
+					UrlBar.Move(cursorY, cursorX+1)
+					UrlBar.Refresh()
 				}
-			}	else if c == nc.KEY_RIGHT {
-					//move one character to the right until the end of the line
-					if cursorX < len(url)+5 {
-						urlBar.Move(cursorY, cursorX+1)
-						urlBar.Refresh()
-					}
-				} else if c == nc.KEY_LEFT {
-						if cursorX > 5 {
-							urlBar.Move(cursorY, cursorX-1)
-							urlBar.Refresh()
-						}
-					} else if c == nc.KEY_HOME {
-						urlBar.Move(cursorY, 5)
-						urlBar.Refresh()
-					} else if c == nc.KEY_END {
-						urlBar.Move(cursorY, len(url)+5)
-						urlBar.Refresh()
-				} else if c > 31 && c < 127 {
-					url += nc.KeyString(c)
-					urlBar.MovePrint(0, 0, "URL: "+url)
+			} else if c == nc.KEY_LEFT {
+				if cursorX > 5 {
+					UrlBar.Move(cursorY, cursorX-1)
+					UrlBar.Refresh()
 				}
-				urlBar.Refresh()
+			} else if c == nc.KEY_HOME {
+				UrlBar.Move(cursorY, 5)
+				UrlBar.Refresh()
+			} else if c == nc.KEY_END {
+				UrlBar.Move(cursorY, len(urlAddr)+5)
+				UrlBar.Refresh()
+			} else if c > 31 && c < 127 {
+				urlAddr += nc.KeyString(c)
+				UrlBar.MovePrint(0, 0, "urlAddr: "+urlAddr)
 			}
+			UrlBar.Refresh()
 		}
-		return url
 	}
+	return urlAddr
 }
 
 func main() {
@@ -116,49 +187,34 @@ func main() {
 	log.SetFormatter(&log.JSONFormatter{})
 	log.SetOutput(f)
 	log.SetLevel(log.InfoLevel)
-	stdscr, err := nc.Init()
-	if err != nil {
-		log.Fatal(err)
-	}
+
 	defer nc.End()
 
-	h, w := stdscr.MaxYX()
-	browserWin, err := nc.NewWindow(h-1, w, 0, 0)
-	if err != nil {
-		log.Fatal(err)
-	}
+	err, browserWin, UrlBar := SetupUI()
 	defer browserWin.Delete()
-	urlBar, err := nc.NewWindow(1, w, h-1, 0)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer urlBar.Delete()
+	defer UrlBar.Delete()
+
+	url := URL{}
 
 	for {
-		urlBar.MovePrint(0, 0, "URL: ")
-		urlBar.Refresh()
-
-		var url string
-		historyCounter := 0
-		history := make([]string, historyCounter)
-		historyIndex := 0
-
-			resp, err := http.Get(url)
-			if err != nil {
-				log.Println(err)
-				return
-			}
-			defer resp.Body.Close()
-			browserWin.Clear()
-			scanner := bufio.NewScanner(resp.Body)
-			for scanner.Scan() {
-				browserWin.MovePrint(0, 0, scanner.Text())
-				browserWin.Refresh()
-			}
-			if err := scanner.Err(); err != nil {
-				log.Println(err)
-				return
-			}
+		UrlBar.MovePrint(0, 0, "URL: ")
+		UrlBar.Refresh()
+		urlAddr := url.GetURL()
+		resp, err := http.Get(urlAddr)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		defer resp.Body.Close()
+		browserWin.Clear()
+		scanner := bufio.NewScanner(resp.Body)
+		for scanner.Scan() {
+			browserWin.MovePrint(0, 0, scanner.Text())
+			browserWin.Refresh()
+		}
+		if err := scanner.Err(); err != nil {
+			log.Println(err)
+			return
 		}
 	}
 }
