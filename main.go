a779aea2240a2f2cabf39184035493fa1f7be289
154pinkchairs <mholub@tutanota.com>
2023-01-05 04:24:20 +0100

    chore(cleanup): move logging to a separate package

--- main.go
+++ main.go
@@ -11,9 +11,9 @@ import (
 	"time"
 
 	hist "github.com/154pinkchairs/gowebcli/history"
+	"github.com/154pinkchairs/gowebcli/logging"
 	nc "github.com/rthornton128/goncurses"
 	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 )
 
 var (
@@ -238,60 +238,23 @@ func (u *URL) GetURLStr(urlStr chan string) string {
 
 func main() {
 	//ctx := context.Background()
-	settings := Settings{}
-	f, err := os.OpenFile("gowebcli.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		fmt.Printf("error opening file: %v", err)
-	}
-	loglevel := os.Getenv("LOG_LEVEL")
-	var zapLevel zapcore.Level
-	switch loglevel {
-	case "trace":
-		zapLevel = zap.DebugLevel
-	case "debug":
-		zapLevel = zap.DebugLevel
-	case "info":
-		zapLevel = zap.InfoLevel
-	case "warn":
-		zapLevel = zap.WarnLevel
-	case "error":
-		zapLevel = zap.ErrorLevel
-	case "fatal":
-		zapLevel = zap.FatalLevel
-	case "panic":
-		zapLevel = zap.PanicLevel
-	default:
-		zapLevel = zap.WarnLevel
-	}
-	jsonEncoder := zap.NewDevelopmentEncoderConfig()
-	//use a human readable time format (01 Jan 06 15:04:13.12345 MST)
-	jsonEncoder.EncodeTime = zapcore.TimeEncoderOfLayout("02 Jan 06 15:04:13.12345 MST")
-	jsonEncoder.EncodeLevel = zapcore.CapitalLevelEncoder
-	jsonEncoder.EncodeCaller = zapcore.ShortCallerEncoder
-	logger := zap.New(zapcore.NewCore(
-		zapcore.NewJSONEncoder(jsonEncoder),
-		zapcore.AddSync(f),
-		zap.NewAtomicLevelAt(zapLevel),
-	),
-		zap.AddCaller(),
-		//zap.AddCallerSkip(1),
-	)
-	Log := logger.Sugar()
+	Log := logging.SetupLogging()
 	hist.SetLogger(Log)
 	Log.Debugf("Assigned logger %v to history package", Log)
 	defer Log.Sync()
 	Log.Info("Starting gowebcli...")
-	DB, err = hist.InitDB()
-	if err != nil {
-		Log.Errorf("Error initializing history database: %v", err)
-		settings.incognito = true
-	} else {
-		//DB.Conn.Conn(ctx)
-		//defer DB.Conn.Close()
-		Log.Debug("Connected to history database")
-		//DB.Mux.Lock()
-		//defer DB.Mux.Unlock()
-	}
+	settings := Settings{}
+	//DB, err := hist.InitDB()
+	//if err != nil {
+	//Log.Errorf("Error initializing history database: %v", err)
+	//settings.incognito = true
+	//} else {
+	//DB.Conn.Conn(ctx)
+	//defer DB.Conn.Close()
+	//Log.Debug("Connected to history database")
+	//DB.Mux.Lock()
+	//defer DB.Mux.Unlock()
+	//}
 	defer nc.End()
 
 	err, browserWin, UrlBar := SetupUI()
